c8867d26408d3e846652f41785176e72d8e04206
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-06-19 22:08:34 +0900

    ✨

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"runtime"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
@@ -18,9 +19,10 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "demo",
-		Width:  1024,
-		Height: 768,
+		Title:     "demo",
+		Width:     1024,
+		Height:    768,
+		Frameless: runtime.GOOS == "windows",
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
