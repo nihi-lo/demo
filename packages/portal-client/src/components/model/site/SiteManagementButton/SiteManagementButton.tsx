bd723fa1fe2bcde34f74a4da8541f6b980527acd
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-06-20 01:04:31 +0900

    ✨

--- packages/portal-client/src/components/model/site/SiteManagementButton/SiteManagementButton.tsx
+++ packages/portal-client/src/components/model/site/SiteManagementButton/SiteManagementButton.tsx
@@ -1,18 +1,7 @@
-import { Tooltip } from "@nextui-org/react";
-
 import { ThemeToggleButton } from "@portal-client/components/model/theme/ThemeToggleButton";
 
 const SiteManagementButton = (): JSX.Element => {
-  return (
-    <Tooltip
-      placement="right"
-      content="管理"
-      closeDelay={0}
-      classNames={{ base: "pointer-events-none select-none" }}
-    >
-      <ThemeToggleButton />
-    </Tooltip>
-  );
+  return <ThemeToggleButton />;
 };
 SiteManagementButton.displayName = "SiteManagementButton";
 
