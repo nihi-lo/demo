78bfd0e027cbb39283728bec1ac9d8ebce4f4592
nihi-lo <115719300+nihi-lo@users.noreply.github.com>
2024-06-26 01:22:01 +0900

    ✨

--- packages/gakusim-app/src/App.tsx
+++ packages/gakusim-app/src/App.tsx
@@ -1,19 +1,16 @@
+import { Image } from "@nextui-org/react";
 import { Routes, Route } from "react-router-dom";
 
 import { type Metadata } from "@portal-core/types";
-import { HStack } from "@portal-core/ui";
 
+import AppIconImage from "@sub-app/gakusim-app/assets/img/AppIcon400x400.png";
 import { TopPage } from "@sub-app/gakusim-app/components/page/TopPage";
 
 const metadata: Metadata = {
   id: "6876b3b6-307d-27ca-d845-6577357297c2",
   title: "学マス - コンテストシミュ",
   description: "",
-  AppIcon: () => (
-    <HStack align="center" justify="center" className="size-12 bg-content3">
-      <p className="text-lg font-semibold text-content3-foreground">学</p>
-    </HStack>
-  ),
+  AppIcon: () => <Image alt="アプリアイコン" radius="none" src={AppIconImage} width={48} />,
 };
 
 const App = (): JSX.Element => {
